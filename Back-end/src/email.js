e39ea5f246928d78822db1c9a8aada91ae3a7ec8
ThiagoMiranda-m <thiagoanm75@gmail.com>
2025-10-24 00:37:50 -0300

    test: email configuration update

--- Back-end/src/email.js
+++ Back-end/src/email.js
@@ -3,7 +3,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
+  port: parseInt(process.env.EMAIL_PORT || '587', 10),
+  secure: process.env.EMAIL_SECURE === 'true',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
